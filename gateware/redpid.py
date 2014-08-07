da006ee28fe137efd2c60870d4aa580f8b4faa93
Robert Jordens <jordens@gmail.com>
2014-08-06 23:56:32 -0600

    redpid standalone

--- gateware/redpid.py
+++ gateware/redpid.py
@@ -4,4 +4,17 @@ from migen.fhdl.std import *
 
 class RedPid(Module):
     def __init__(self, platform):
-        pass
+        exp = platform.request("exp")
+        #xadc = platform.request("xadc")
+        dac = platform.request("dac")
+        adc0 = platform.request("adc", 0)
+        adc1 = platform.request("adc", 1)
+        adc_clk = platform.request("adc_clk")
+        self.comb += [
+                exp.raw_bits().eq(0),
+                #xadc.raw_bits().eq(0),
+                dac.raw_bits().eq(0),
+                adc0.eq(0),
+                adc1.eq(0),
+                adc_clk.eq(0),
+        ]
