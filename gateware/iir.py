e1704d39938fe07052fc1363d47b42d2ea9c6309
Robert Jordens <jordens@gmail.com>
2014-08-12 05:14:57 -0600

    iir work

--- gateware/iir.py
+++ gateware/iir.py
@@ -1,66 +1,87 @@
+import math
+
 from migen.fhdl.std import *
-from math import pi
+from migen.bank.description import CSRStorage, CSRStatus, AutoCSR
+
+from migen.fhdl.bitcontainer import bits_for
+from migen.bus.csr import Initiator
+from migen.bank.csrgen import get_offset, Bank
+from migen.bus.transactions import TWrite
 
 
-class Iir(Module):
-    def __init__(self, order=2, mode="pipelined", x=16, y=16,
-            width=18, a0_shift=16, intermediate_width=48, wait=4):
+class Iir(Module, AutoCSR):
+    def __init__(self, order=2, mode="pipelined",
+            signal_width=25, coeff_width=18, intermediate_width=48,
+            wait=4):
         assert mode in ("pipelined", "iterative")
 
-        if type(x) is type(0):
-            x = Signal((x, True))
-        if type(y) is type(0):
-            y = Signal((y, True))
-        self.x = x
-        self.y = y
-        x = Signal((width, True))
-        y = Signal((width, True))
-        self.bypass = Signal()
-        
+        self.x = Signal((signal_width, True))
+        self.y = Signal((signal_width, True))
+        self.mode_in = Signal(2) # hold, clear
+        self.mode_out = Signal(2)
+
+        self.r_cmd = CSRStorage(2)
+        self.r_mask = CSRStorage(2)
+        self.r_mode = CSRStatus(2)
+        self.r_bias = CSRStorage(intermediate_width)
+
+        self.c = c = {}
+        for i in "ab":
+            for j in range(order + 1):
+                name = "%s%i" % (i, j)
+                if name == "a0":
+                    ci = Signal(max=intermediate_width)
+                else:
+                    ci = Signal((coeff_width, True))
+                rci = CSRStorage(flen(ci), name=name)
+                self.comb += ci.eq(rci.storage)
+                c[name] = ci
+                setattr(self, "r_" + name, rci)
+
+        ###
+
+        yn = Signal((signal_width + 1, True))
+        yo = Signal((signal_width, True))
+        self.sync += yo.eq(self.y)
+        rail = Signal()
+        mode = Signal(2)
         self.comb += [
-                x.eq(self.x << (width-flen(self.x))),
-                self.y.eq(y >> (width-flen(self.y))),
-                If(self.bypass, (
-                    self.y.eq(self.x >> (flen(self.x)-flen(self.y)))
-                        if flen(self.x) > flen(self.y) else
-                    self.y.eq(self.x << (flen(self.y)-flen(self.x)))),
+                self.mode_out.eq((self.mode_in & ~self.r_mask.storage) |
+                    self.r_cmd.storage | Cat(rail, 0)),
+                self.r_mode.status.eq(self.mode_out),
+                rail.eq(yn[-2] != yn[-1]),
+                If(rail,
+                    self.y.eq(yo),
+                ).Else(
+                    self.y.eq(yn),
                 )]
 
-        b = [Signal((width, True), "b{}".format(i))
-                for i in range(order+1)]
-        a = [Signal((width, True), "a{}".format(i))
-                for i in range(1, order+1)]
-        self.coeffs = dict((s.backtrace[-1][0], s) for s in a+b)
-        yn = Signal((width, True))
-        self.a0_shift = a0_shift
-        self.stop = Signal()
-
         if mode == "pipelined":
-            self.latency = 1
+            self.latency = 2
             self.interval = 1
-            ib = [Signal((intermediate_width, True), "ib{}".format(i))
-                    for i in range(order+1)] + [0]
-            ia = [Signal((intermediate_width, True), "ia{}".format(i))
-                    for i in range(1, order+1)] + [ib[0]]
-            self.sync += [ib[i].eq(b[i]*x + ib[i+1]) for i in range(order+1)]
-            self.sync += [ia[i].eq(a[i]*y + ia[i+1]) for i in range(order)]
-            self.comb += yn.eq(ia[0] >> a0_shift)
-            self.sync += [
-                    If(self.stop,
-                        [iabi.eq(0) for iabi in ib[:-1] + ia[:-1]],
-                    )]
+            stage = Signal((intermediate_width, True))
+            self.sync += stage.eq(self.r_bias.storage)
+            stages = [stage]
+            r = [("b%i" % i, self.x) for i in reversed(range(order + 1))]
+            r += [("a%i" % i, self.y) for i in reversed(range(1, order + 1))]
+            for i, (coeff, sig) in enumerate(r):
+                stage, _stage = Signal.like(stage), stage
+                stages.append(stage)
+                self.sync += If(~self.mode_out[0], stage.eq(c[coeff]*sig + _stage))
+            self.comb += yn.eq(stage >> c["a0"])
+            self.sync += If(self.mode_out[1], [i.eq(0) for i in stages])
 
         elif mode == "iterative":
             self.latency = (2*order+1)*wait
             self.interval = self.latency
-            ma = Signal((width, True))
-            mb = Signal((width, True))
+            ma = Signal((signal_width, True))
+            mb = Signal((coeff_width, True))
             mc = Signal((intermediate_width, True))
             mp = Signal((intermediate_width, True))
             self.comb += mp.eq(ma*mb + mc)
 
-            xx = [x] + [Signal((width, True)) for i in range(order)]
-            yy = [y] + [Signal((width, True)) for i in range(order-1)]
+            xx = [x] + [Signal((signal_width, True)) for i in range(order)]
+            yy = [y] + [Signal((signal_width, True)) for i in range(order-1)]
             muls = []
             muls += [[b[i], xx[i], mp] for i in range(order+1)]
             muls += [[a[i], yy[i], mp] for i in range(order)]
@@ -98,81 +119,76 @@ class Iir(Module):
                         Cat(yn, *yy).eq(0),
                     )]
 
-        inc = Signal()
-        dec = Signal()
-        wound_up = Signal()
-        self.hold = Signal()
-        self.railed = Signal(2)
-        yo = Signal((width, True))
-        self.sync += yo.eq(y)
-        self.comb += [
-                inc.eq(yn > yo),
-                dec.eq(yn < yo),
-                wound_up.eq((self.railed[0] & inc) | (self.railed[1] & dec)),
-                If(wound_up | self.hold,
-                    y.eq(yo),
-                ).Else(
-                    y.eq(yn),
-                )]
 
 
 class TB(Module):
     def __init__(self, gen=[], params={}, **kwargs):
         self.submodules.iir = Iir(**kwargs)
-        self.params = params
-        self.gen = gen
+        self.desc = self.iir.get_csrs()
+        self.submodules.bank = Bank(self.desc)
+        self.submodules.init = Initiator(self.writes(params),
+                self.bank.bus)
+        self.gen = iter(gen)
         self.x = []
         self.y = []
 
-    def do_simulation(self, s):
-        if s.cycle_counter == 0:
-            for k, v in self.params.items():
-                p = self.iir.coeffs[k]
-                vs = int(round(v*2**(self.iir.a0_shift)))
-                vp = vs&((1<<flen(p))-1)
-                print(k, v, vs, vp)
-                s.wr(p, vp)
-        else:
-            for xi in self.gen:
-                self.x.append(xi)
-                xi *= 2**(flen(self.iir.x)-1)
-                s.wr(self.iir.x, xi)
-                yi = s.rd(self.iir.y)/2**(flen(self.iir.y)-1)
-                self.y.append(yi)
-                break
+    def writes(self, params):
+        for k in sorted(params):
+            n = flen(self.iir.c[k])
+            v = params[k]
+            print(k, v, hex(v))
+            for i in range(0, n, 8):
+                adr = get_offset(self.desc, k) + (n - i + 8 - 1)//8 - 1
+                yield TWrite(adr, (v >> i) & 0xff)
+
+    def do_simulation(self, selfp):
+        try:
+            xi = next(self.gen)
+        except StopIteration:
+            raise StopSimulation
+        self.x.append(xi)
+        xi = xi*2**(flen(self.iir.x)-1)
+        selfp.iir.x = xi
+        yi = selfp.iir.y
+        yi = yi/2**(flen(self.iir.y)-1)
+        self.y.append(yi)
 
 
-def get_params(typ="pi", ts=100e6, f0=1., k=1., g=1.):
+def get_params(typ="pi", f=1., k=1., g=1., shift=None, width=25, fs=1.):
+    f *= math.pi/fs
     if typ == "pi":
-        return {
-                "a1": (1-pi*f0*ts/g)/(1+pi*f0*ts/g),
-                "b0": k*(1+pi*f0*ts)/(1+pi*f0*ts/g),
-                "b1": -k*(1-pi*f0*ts)/(1+pi*f0*ts/g),
+        p = {
+                "a1": (1 - f/g)/(1 + f/g),
+                "b0": k*(1 + f)/(1 + f/g),
+                "b1": -k*(1 - f)/(1 + f/g),
                 }
+    if shift is None:
+        shift = width + 1 - max(bits_for(v, require_sign_bit=True)
+                for v in p.values())
+    for k in p:
+        p[k] = int(p[k]*2**shift)
+    p["a0"] = shift
+    return p
 
 
 
 def main():
     from migen.fhdl import verilog
-    from migen.sim.generic import Simulator, TopLevel
+    from migen.sim.generic import run_simulation
     from matplotlib import pyplot as plt
     import numpy as np
 
-    iir = Iir(width=35, intermediate_width=70, a0_shift=26,
-            order=2, mode="iterative")
-    print(verilog.convert(iir, ios={iir.x, iir.y,
-        iir.hold, iir.railed, iir.bypass, iir.stop} |
-        set(iir.coeffs.values())))
+    #iir = Iir()
+    #print(verilog.convert(iir, ios={iir.x, iir.y,
+    #    iir.mode_in, iir.mode_out}))
 
-    n = 10000
+    n = 1000
     x = np.zeros(n)
-    x[:n/2] = 1e-3
-    p = get_params("pi", f0=1e1, ts=1e3*10, k=1e-4, g=1e7)
-    tb = TB(iter(x), p, width=35, intermediate_width=70,
-            a0_shift=26, order=1, mode="iterative")
+    x[100:n/2] = 1e-3
+    p = get_params("pi", f=1e-3, k=1e-2, g=1e9)
+    tb = TB(x, p, order=1, mode="pipelined")
     #print(verilog.convert(tb))
-    sim = Simulator(tb, TopLevel("iir.vcd"))
-    sim.run(n)
+    run_simulation(tb, vcd_name="iir.vcd")
     plt.plot(tb.x)
     plt.plot(tb.y)
     plt.show()
