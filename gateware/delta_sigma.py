6848257b9b7d4333c5725664ef161583771998be
Robert Jordens <jordens@gmail.com>
2014-08-17 16:39:20 -0600

    add delta sigma dac

---
+++ gateware/delta_sigma.py
@@ -0,0 +1,47 @@
+from migen.fhdl.std import *
+from migen.sim.generic import run_simulation, StopSimulation
+
+
+class DeltaSigma(Module):
+    def __init__(self, width=24):
+        self.data = Signal(width)
+        self.out = Signal()
+
+        ###
+
+        delta = Signal(2)
+        sigma = Signal(width + 2)
+        self.comb += delta.eq(Cat(sigma[-1], sigma[-1]))
+        self.sync += sigma.eq((self.data + (delta << width)) + sigma)
+        self.comb += self.out.eq(sigma[-1])
+
+
+class TB(Module):
+    def __init__(self, **kwargs):
+        self.submodules.dut = DeltaSigma(**kwargs)
+        n = 1<<flen(self.dut.data)
+        self.x = [j for j in range(n) for i in range(n)]
+        self.y = []
+        self.gen = iter(self.x)
+
+    def do_simulation(self, selfp):
+        try:
+            selfp.dut.data = next(self.gen)
+        except StopIteration:
+            pass
+        self.y.append(selfp.dut.out)
+        if len(self.y) == len(self.x):
+            raise StopSimulation
+
+
+if __name__ == "__main__":
+    import numpy as np
+    import matplotlib.pyplot as plt
+    tb = TB(width=8)
+    run_simulation(tb)
+    x, y = np.array(tb.x), np.array(tb.y)
+    n = 1<<flen(tb.dut.data)
+    plt.plot(x.reshape(-1, n).mean(1))
+    plt.plot(y.reshape(-1, n).mean(1)*n)
+    #plt.plot(np.repeat(n
+    plt.show()
