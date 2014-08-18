4574d66f63e3f96410c1f1cd967b6d60ae8e3784
Robert Jordens <jordens@gmail.com>
2014-08-17 18:35:26 -0600

    csv headers

--- make.py
+++ make.py
@@ -1,5 +1,7 @@
 # Robert Jordens <jordens@gmail.com> 2014
 
+from migen.fhdl.std import *
+
 from gateware.platform import Platform
 from gateware.redpid import RedPid
 
@@ -7,5 +9,17 @@ from gateware.redpid import RedPid
 if __name__ == "__main__":
     platform = Platform()
     redpid = RedPid(platform)
+
+    csr_base = 0x40300000
+    for name, csrs, mapaddr, rmap in redpid.pid.csrbanks.banks:
+        reg_base = csr_base + 0x800*mapaddr
+        busword = flen(rmap.bus.dat_w)
+        for csr in csrs:
+            nr = (csr.size + busword - 1)//busword
+            r = "{}_{},0x{:08x},{},{}".format(name, csr.name, reg_base, nr,
+                    "ro" if hasattr(csr, "status") else "rw")
+            print(r)
+            reg_base += 4*nr
+
     platform.add_source_dir("verilog")
     platform.build_cmdline(redpid, build_name="redpid")
