b7dec1a095f2c16a555d8c4492f3afd2ef3e4235
Robert Jordens <jordens@gmail.com>
2014-08-26 20:40:22 -0600

    fix dna

--- gateware/dna.py
+++ gateware/dna.py
@@ -9,21 +9,26 @@ class DNA(Module, AutoCSR):
         ###
 
         d = Signal()
-        cnt = Signal(max=flen(self.r_dna.status) + 1)
+        n = flen(self.r_dna.status)
         rd = Signal(reset=1)
-        shift = Signal()
+        so = Signal()
+        si = Signal()
+        cnt = Signal(max=n + 1, reset=n)
 
         self.specials += Instance("DNA_PORT",
-                i_DIN=0, o_DOUT=d, i_CLK=ClockSignal(),
-                i_READ=rd, i_SHIFT=shift)
+                i_DIN=d, o_DOUT=d, i_CLK=ClockSignal(),
+                i_READ=rd, i_SHIFT=so)
 
+        self.comb += so.eq(~rd & (cnt > 0))
         self.sync += [
                 If(rd,
-                    rd.eq(0),
-                    shift.eq(1)
+                    rd.eq(0)
                 ),
-                If(shift & (cnt < flen(self.r_dna.status)),
-                    self.r_dna.status.eq(Cat(d, self.r_dna.status)),
-                    cnt.eq(cnt + 1)
+                If(so,
+                    cnt.eq(cnt - 1),
+                ),
+                si.eq(so),
+                If(si,
+                    self.r_dna.status.eq(Cat(self.r_dna.status[1:], d)),
                 )
         ]
