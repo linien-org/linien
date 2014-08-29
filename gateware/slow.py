24bde150cf21d3c2d9f747422cc2fdd94035a4d0
Robert Jordens <jordens@gmail.com>
2014-08-29 00:11:20 -0600

    gpio: expose in and out as signals

--- gateware/slow.py
+++ gateware/slow.py
@@ -10,6 +10,8 @@ from .dna import DNA
 class Gpio(Module, AutoCSR):
     def __init__(self, pins):
         n = flen(pins)
+        self.i = Signal(n)
+        self.o = Signal(n)
         self._r_in = CSRStatus(n)
         self._r_out = CSRStorage(n)
         self._r_oe = CSRStorage(n)
@@ -18,10 +20,11 @@ class Gpio(Module, AutoCSR):
 
         t = [TSTriple(1) for i in range(n)]
         self.specials += [ti.get_tristate(pins[i]) for i, ti in enumerate(t)]
-        self.specials += MultiReg(Cat([ti.i for ti in t]), self._r_in.status)
+        self.specials += MultiReg(Cat([ti.i for ti in t]), self.i)
         self.comb += [
-                Cat([ti.o for ti in t]).eq(self._r_out.storage),
+                Cat([ti.o for ti in t]).eq(self._r_out.storage | self.o),
                 Cat([ti.oe for ti in t]).eq(self._r_oe.storage),
+                self._r_in.status.eq(self.i),
         ]
 
 
