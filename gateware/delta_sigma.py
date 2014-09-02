cd678a1b342c3f93f6dcac29202a17fb9f62ad17
Robert Jordens <jordens@gmail.com>
2014-09-02 03:09:36 -0600

    second order delta sigma

--- gateware/delta_sigma.py
+++ gateware/delta_sigma.py
@@ -10,11 +10,35 @@ class DeltaSigma(Module):
 
         ###
 
-        delta = Signal(2)
-        sigma = Signal(width + 2)
-        self.comb += delta.eq(Cat(sigma[-1], sigma[-1]))
-        self.sync += sigma.eq((self.data + (delta << width)) + sigma)
+        delta = Signal(width + 1)
+        sigma = Signal(width + 1)
+        self.comb += delta.eq(self.out << width)
+        self.sync += sigma.eq(self.data - delta + sigma)
         self.comb += self.out.eq(sigma[-1])
+
+
+class DeltaSigma2(Module):
+    def __init__(self, width=24):
+        self.data = Signal(width)
+        self.out = Signal()
+
+        ###
+
+        delta = Signal(width + 1)
+        i1 = Signal(width + 2)
+        i2 = Signal(width + 2)
+        sigma1 = Signal(width + 2)
+        sigma2 = Signal(width + 2)
+        self.comb += [
+                delta.eq(self.out << width),
+                i1.eq(self.data - delta + sigma1),
+                i2.eq(i1 - delta + sigma2),
+                self.out.eq(sigma2[-1])
+        ]
+        self.sync += [
+                sigma1.eq(i1),
+                sigma2.eq(i2)
+        ]
 
 
 class DeltaSigmaCSR(Module, AutoCSR):
@@ -29,8 +53,8 @@ class DeltaSigmaCSR(Module, AutoCSR):
 
 
 class TB(Module):
-    def __init__(self, **kwargs):
-        self.submodules.dut = DeltaSigma(**kwargs)
+    def __init__(self, dut):
+        self.submodules.dut = dut
         n = 1<<flen(self.dut.data)
         self.x = [j for j in range(n) for i in range(n)]
         self.y = []
@@ -42,18 +66,20 @@ class TB(Module):
         except StopIteration:
             pass
         self.y.append(selfp.dut.out)
-        if len(self.y) == len(self.x):
+        if len(self.y) - 2 == len(self.x):
+            del self.y[:2]
             raise StopSimulation
 
 
 if __name__ == "__main__":
     import numpy as np
     import matplotlib.pyplot as plt
-    tb = TB(width=8)
+    dut = DeltaSigma(8)
+    tb = TB(dut)
     run_simulation(tb)
     n = 1<<flen(tb.dut.data)
     x = np.array(tb.x).reshape(-1, n)
     y = np.array(tb.y).reshape(-1, n)
-    #plt.plot(x[:, 0], x[:, 0] - y.sum(1))
-    plt.plot(y.ravel())
+    plt.plot(x[:, 0], x[:, 0] - y.sum(1))
+    #plt.plot(y.ravel())
     plt.show()
