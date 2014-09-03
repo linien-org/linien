b7c0afeb1220a47a899332c22c398d6acb957643
Robert Jordens <jordens@gmail.com>
2014-09-02 18:15:48 -0600

    refactor bit2bin, cleanup

--- make.py
+++ make.py
@@ -5,7 +5,7 @@ from migen.fhdl.std import *
 
 from gateware.platform import Platform
 from gateware.redpid import RedPid
-
+from bit2bin import bit2bin
 
 def get_csrmap(banks):
     csr_base = 0x40300000
@@ -28,6 +28,7 @@ if __name__ == "__main__":
     redpid = RedPid(platform)
     platform.add_source_dir("verilog")
     platform.build_cmdline(redpid, build_name="redpid")
+    bit2bin("build/redpid.bit", "build/redpid.bin", flip=True)
 
     fil = open("test/csrmap.py", "w")
     csr = get_csrmap(redpid.pid.csrbanks.banks)
