4405765dd18f4e8c7f8c0b55732274b9ebd4fb5a
Robert Jordens <jordens@gmail.com>
2015-09-03 12:04:44 -0600

    add license boilerplate

--- gateware/gpio.py
+++ gateware/gpio.py
@@ -1,4 +1,19 @@
-# (C) Roberr Jordens <jordens@gmail.com> 2014, 2015
+# Copyright 2014-2015 Robert Jordens <jordens@gmail.com>
+#
+# This file is part of redpid.
+#
+# redpid is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# redpid is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with redpid.  If not, see <http://www.gnu.org/licenses/>.
 
 from migen.fhdl.std import *
 from migen.bank.description import *
