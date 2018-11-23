cfa3026165321e61c96b28800b1705623f09c70c
Benjamin Wiegand <highwaychile@zoho.com>
2018-11-23 16:15:40 +0100

    use "top" as module name

--- make.py
+++ make.py
@@ -59,5 +59,5 @@ if __name__ == "__main__":
     fil.close()
 
     platform.add_source_dir("verilog")
-    platform.build(redpid, build_name="redpid")
+    platform.build(redpid, build_name="top")
     bit2bin("build/redpid.bit", "build/redpid.bin", flip=True)
