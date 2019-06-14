7f4805125720f404070f0916135e70697e13c438
Benjamin Wiegand <highwaychile@zoho.com>
2019-06-14 10:49:43 +0200

    cleanup server dependencies

--- linien/server/install_on_device.sh
+++ linien/server/install_on_device.sh
@@ -1,26 +1,21 @@
+# the server is started in a screen session
 apt-get install screen
-pip3 install rpyc
+pip3 install rpyc myhdl
 
-
+# monitoradvanced is required for manipulating FPGA registers
+cd /tmp
 git clone https://github.com/RedPitaya/RedPitaya.git
 cd RedPitaya/Test/monitoradvanced
 make
 cp monitoradvanced /usr/bin
 
 # install pyrp3
+cd /tmp
 git clone https://github.com/hermitdemschoenenleben/pyrp3.git
 cd pyrp3
 python3 setup.py install
 cd ../
 pip3 install myhdl
-
-# install pyrp2
-git clone https://github.com/hermitdemschoenenleben/pyrp.git
-cd pyrp
-python2.7 setup.py install
-apt-get install python-pip python-numpy
-#wget https://bootstrap.pypa.io/ez_setup.py -O - | python
-#pip2 install myhdl
 
 # build monitor shared library
 cd monitor
