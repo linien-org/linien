abb9876a87d121f0d2aae73c35b57fa85895b453
Benjamin Wiegand <highwaychile@zoho.com>
2019-07-29 11:13:35 +0200

    implement data export

--- linien/client/ui/view_panel.py
+++ linien/client/ui/view_panel.py
@@ -1,5 +1,8 @@
+import json
+import pickle
 import numpy as np
-from PyQt5 import QtGui
+from os import path
+from PyQt5 import QtGui, QtWidgets
 
 from linien.client.utils import param2ui
 from linien.client.widgets import CustomWidget
@@ -13,6 +16,9 @@ class ViewPanel(QtGui.QWidget, CustomWidget):
         self.ids.autoscale_y_axis.stateChanged.connect(self.autoscale_changed)
         self.ids.y_axis_minimum.valueChanged.connect(self.y_limits_changed)
         self.ids.y_axis_maximum.valueChanged.connect(self.y_limits_changed)
+
+        self.ids.export_select_file.clicked.connect(self.export_select_file)
+        self.ids.export_data.clicked.connect(self.export_data)
 
     def connection_established(self):
         params = self.app().parameters
@@ -35,3 +41,42 @@ class ViewPanel(QtGui.QWidget, CustomWidget):
             self.ids.y_axis_minimum.value(),
             self.ids.y_axis_maximum.value()
         )
+
+    def export_select_file(self):
+        options = QtWidgets.QFileDialog.Options()
+        #options |= QtWidgets.QFileDialog.DontUseNativeDialog
+        default_ext = '.json'
+        fn, _ = QtWidgets.QFileDialog.getSaveFileName(self,"QFileDialog.getSaveFileName()","","JSON (*%s)" % default_ext, options=options)
+        if fn:
+            if not fn.endswith(default_ext):
+                fn = fn + default_ext
+            self.export_fn = fn
+            self.ids.export_select_file.setText('File selected: %s' % path.split(fn)[-1])
+            self.ids.export_data.setEnabled(True)
+
+    def export_data(self):
+        fn = self.export_fn
+        counter = 0
+
+        while True:
+            if counter > 0:
+                name, ext = path.splitext(fn)
+                fn_with_suffix = name + '-' + str(counter)
+                if ext:
+                    fn_with_suffix += ext
+            else:
+                fn_with_suffix = fn
+
+            try:
+                open(fn_with_suffix, 'r')
+                counter += 1
+                continue
+            except FileNotFoundError:
+                break
+
+        print('export data to', fn_with_suffix)
+
+        with open(fn_with_suffix, 'w') as f:
+            data = dict(self.parameters)
+            data['to_plot'] = pickle.loads(data['to_plot'])
+            json.dump(data, f)
