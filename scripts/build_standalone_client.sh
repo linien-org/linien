376289a75deb8b286da8c58ad400bbc4b30b1a88
Benjamin Wiegand <highwaychile@zoho.com>
2019-09-24 15:34:43 +0200

    improve scripts

--- scripts/build_standalone_client.sh
+++ scripts/build_standalone_client.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 cd ..
 
+while true; do
+    read -p "Did you update your VERSION file?" yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
 pyinstaller client.spec
