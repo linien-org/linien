dfc44ab57718a11a71175ccc10ce2b9257293d05
Benjamin Wiegand <highwaychile@posteo.de>
2020-09-18 14:51:38 +0200

    fix build for recent migen versions

--- scripts/build_gateware.sh
+++ scripts/build_gateware.sh
@@ -5,6 +5,15 @@ SCRIPT=`realpath $0`
 SCRIPTPATH=`dirname $SCRIPT`
 cd $SCRIPTPATH/../
 
+VIVADOPATH=/tools/Xilinx/Vivado/2020.1/bin
+if [ ! -d $VIVADOPATH ]
+then
+echo "vivado path $VIVADOPATH does not exist. Please adapt it in build_gateware.sh"
+exit 1
+fi
+
+export PATH=$VIVADOPATH:$PATH
+
 python3 make.py
 
 rm linien/server/linien.bin -f
