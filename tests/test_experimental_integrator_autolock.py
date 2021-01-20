36f47b2e3381b5451f94b4033db07b41dccd0f85
Benjamin Wiegand <highwaychile@posteo.de>
2021-01-20 18:08:19 +0100

    cleanup

--- tests/test_experimental_integrator_autolock.py
+++ tests/test_experimental_integrator_autolock.py
@@ -1,28 +1,7 @@
-import math
 import numpy as np
 from matplotlib import pyplot as plt
-from migen import (
-    Signal,
-    Module,
-    Instance,
-    ClockSignal,
-    ResetSignal,
-    Array,
-    Record,
-    ClockDomain,
-    ClockDomainsRenamer,
-    If,
-    bits_for,
-)
-from misoc.interconnect.csr import AutoCSR, CSRStorage, CSRStatus
-
 
 TARGET_IDXS = (328, 350)
-DEFAULT_SCALE_FACTOR = 0.5
-COND_LT = 0
-COND_GT = 1
-
-LENGHT_TOLERANCE_FACTOR = 0.5
 
 
 def peak(x):
@@ -40,24 +19,23 @@ def add_noise(spectrum, level=100):
     return spectrum + (np.random.randn(len(spectrum)) * level)
 
 
-def get_xscale(spectrum, target_idxs):
+def get_x_scale(spectrum, target_idxs):
     part = spectrum[target_idxs[0] : target_idxs[1]]
-
     return np.abs(np.argmin(part) - np.argmax(part))
 
 
-def sum_up(spectrum):
+def sum_up_spectrum(spectrum):
     sum_ = 0
     summed = []
 
-    for idx, value in enumerate(spectrum):
+    for value in spectrum:
         summed.append(sum_ + value)
         sum_ += value
 
     return summed
 
 
-def sum_at_xscale(summed, xscale):
+def get_diff_at_x_scale(summed, xscale):
     new = []
 
     for idx, value in enumerate(summed):
@@ -75,7 +53,7 @@ def sign(value):
     return 1 if value >= 1 else -1
 
 
-def get_left_peak(summed_xscaled, target_idxs):
+def get_target_peak(summed_xscaled, target_idxs):
     selected_region = summed_xscaled[target_idxs[0] : target_idxs[1]]
     # in the selected region, we may have 1 minimum and one maximum
     # we know that we are interested in the "left" extremum --> sort extrema
@@ -85,13 +63,12 @@ def get_left_peak(summed_xscaled, target_idxs):
     return current_idx
 
 
-def get_peaks(summed_xscaled, target_idxs):
-    current_idx = get_left_peak(summed_xscaled, target_idxs)
+def get_all_peaks(summed_xscaled, target_idxs):
+    current_idx = get_target_peak(summed_xscaled, target_idxs)
 
     peaks = []
 
     peaks.append(summed_xscaled[current_idx])
-    yscale = np.abs(peaks[-1])
 
     while True:
         if current_idx == 0:
@@ -110,12 +87,13 @@ def get_peaks(summed_xscaled, target_idxs):
     return peaks
 
 
-def get_lock_position_from_description(spectrum, description):
+def get_lock_position_from_description(spectrum, description, x_scale):
+    summed = sum_up_spectrum(spectrum)
+    summed_xscaled = get_diff_at_x_scale(summed, x_scale)
+
     description_idx = 0
 
-    print(description)
-
-    for idx, value in enumerate(spectrum):
+    for idx, value in enumerate(summed_xscaled):
         current_threshold = description[description_idx]
 
         if sign(value) == sign(current_threshold) and abs(value) >= abs(
@@ -129,83 +107,77 @@ def get_lock_position_from_description(spectrum, description):
 
     plt.clf()
     plt.plot(spectrum)
+    plt.plot(summed_xscaled)
     plt.show()
     raise Exception("not found")
 
 
-def test_get_description():
-    spectrum = spectrum_for_testing(0)
-
-    initial_spectra = []
-    for idx in range(10):
-        initial_spectra.append(add_noise(spectrum))
-
-    xscales = []
-    for initial_spectrum in initial_spectra:
-        xscales.append(get_xscale(initial_spectrum, TARGET_IDXS))
-    xscale = int(round(np.mean(xscales)))
-    print(xscale)
+def get_description(spectra, target_idxs):
+    x_scale = int(
+        round(np.mean([get_x_scale(spectrum, target_idxs) for spectrum in spectra]))
+    )
+    print(f"x scale is {x_scale}")
 
     all_peaks = []
 
-    for initial_spectrum in initial_spectra:
-        plt.plot(initial_spectrum, label="initial")
-        summed = sum_up(initial_spectrum)
-        summed_xscaled = sum_at_xscale(summed, xscale)
-        # plt.plot(summed, label="summed")
-        plt.plot([_ / xscale for _ in summed_xscaled], label="summed_xscaled / xscale")
-
-        peaks = get_peaks(summed_xscaled, TARGET_IDXS)
+    for spectrum in spectra:
+        prepared_spectrum = get_diff_at_x_scale(sum_up_spectrum(spectrum), x_scale)
+        peaks = get_all_peaks(prepared_spectrum, target_idxs)
         all_peaks.append(peaks)
 
-    first_peaks = [peaks[0] for peaks in all_peaks]
-    first_peak_mean = np.mean(first_peaks)
-    first_peak_std = np.std(first_peaks)
-
+    target_peaks = [peaks[0] for peaks in all_peaks]
     # TODO: Think about it. sigma=2 should be 95 % confidence. Handle the case that the difference two lines below is negative
     sigma_factor = 4
+    noise_level = sigma_factor * np.std(target_peaks)
 
-    noise_level = first_peak_std * sigma_factor
-    print("NOISE", noise_level)
+    print(f"noise level is {noise_level}")
 
     description = [
         # FIXME: take care that (1-XXX) is never negative (or even < .5)
         peak * (1 - abs(noise_level / peak))
         for peak in all_peaks[0]
     ]
+    # it is important to do the filtering that happens here after the previous
+    # line as the previous line shrinks the values
     description = [peak for peak in description if abs(peak) > abs(noise_level)]
-    description = list(reversed(description))
 
     # now find out how much we have to wait in the end (because we detect the peak
     # too early because our threshold is too low)
-    last_peak_description_height = description[-1]
-    initial_spectrum = initial_spectra[0]
-    summed = sum_up(initial_spectrum)
-    summed_xscaled = sum_at_xscale(summed, xscale)
-    real_peak_idx = get_left_peak(summed_xscaled, TARGET_IDXS)
-    current_idx = real_peak_idx
+    target_peak_described_height = description[0]
+    initial_spectrum = spectra[0]
+    prepared_spectrum = get_diff_at_x_scale(sum_up_spectrum(initial_spectrum), x_scale)
+    target_peak_idx = get_target_peak(prepared_spectrum, TARGET_IDXS)
+    current_idx = target_peak_idx
     while True:
         current_idx -= 1
-        if np.abs(summed_xscaled[current_idx]) < np.abs(last_peak_description_height):
+        if np.abs(prepared_spectrum[current_idx]) < np.abs(
+            target_peak_described_height
+        ):
             break
-    final_wait_time = real_peak_idx - current_idx
-    print("FINAL", final_wait_time)
+    final_wait_time = target_peak_idx - current_idx
+    print(f"final wait time is {final_wait_time} samples")
 
-    print(np.array(description) / xscale)
+    return list(reversed(description)), final_wait_time, x_scale
+
+
+def test_get_description():
+    spectrum = spectrum_for_testing(0)
+
+    spectra = [add_noise(spectrum) for _ in range(10)]
+
+    description, final_wait_time, x_scale = get_description(spectra, TARGET_IDXS)
 
     lock_positions = []
-    for initial_spectrum in initial_spectra:
-        summed = sum_up(initial_spectrum)
-        summed_xscaled = sum_at_xscale(summed, xscale)
 
+    for spectrum in spectra:
         lock_positions.append(
-            get_lock_position_from_description(summed_xscaled, description)
+            get_lock_position_from_description(spectrum, description, x_scale)
             + final_wait_time
         )
 
-        plt.axvline(lock_positions[-1])
-        break
+        plt.axvline(lock_positions[-1], color="green", alpha=0.5)
 
+    plt.plot(spectra[0])
     plt.axvspan(TARGET_IDXS[0], TARGET_IDXS[1], alpha=0.2, color="red")
 
     plt.legend()
