0b12b4c673f2f345bde3ec6ac27f515fda9f1af1
Bastian Leykauf <leykauf@physik.hu-berlin.de>
2022-02-10 16:42:01 +0100

    Add possibility to check version from future repo location.

--- linien/gui/ui/version_checker.py
+++ linien/gui/ui/version_checker.py
@@ -1,3 +1,5 @@
+import json
+from urllib.error import HTTPError
 from urllib.request import urlopen
 
 from packaging import version
@@ -10,15 +12,19 @@ class VersionCheckerThread(QThread):
     check_done = pyqtSignal(bool)
 
     def run(self):
-        new_version_available = False
+        our_version = version.parse(linien.__version__)
+        latest_version = our_version
         print("Check whether new version is available.")
+        url_legacy = "https://raw.githubusercontent.com/hermitdemschoenenleben/linien/master/latest_version"  # noqa: E501
+        url = "https://raw.githubusercontent.com/linien-org/linien/master/version-info.json"  # noqa: E501
         try:
-            with urlopen(
-                "https://raw.githubusercontent.com/hermitdemschoenenleben/linien/master/latest_version"  # noqa: E501
-            ) as response:
+            with urlopen(url_legacy) as response:
                 response_content = response.read().decode().strip()
                 latest_version = version.parse(response_content)
-                our_version = version.parse(linien.__version__)
-                new_version_available = latest_version > our_version
+        except HTTPError:
+            # after repo is moved to new location
+            with urlopen(url) as response:
+                response_content = json.loads(response.read())
+                latest_version = version.parse(response_content["latest"])
         finally:
-            self.check_done.emit(new_version_available)
+            self.check_done.emit(latest_version > our_version)
