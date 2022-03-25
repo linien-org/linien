73bf9153d3fd78d0b1ddb5f18a62b013a1fd8f79
Bastian Leykauf <leykauf@physik.hu-berlin.de>
2022-03-25 16:38:57 +0100

    Add missing copyright notice to server.

--- linien/server/iir_coeffs.py
+++ linien/server/iir_coeffs.py
@@ -1,4 +1,6 @@
 # Copyright 2014-2015 Robert Jordens <jordens@gmail.com>
+# Copyright 2018-2022 Benjamin Wiegand <benjamin.wiegand@physik.hu-berlin.de>
+# Copyright 2021-2022 Bastian Leykauf <leykauf@physik.hu-berlin.de>
 #
 # This file is part of redpid.
 #
