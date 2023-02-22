50609aec4fbe0bb043beb16b073b73981e903025
Bastian Leykauf <leykauf@physik.hu-berlin.de>
2023-02-22 13:34:09 +0100

    add deprication warnings to description

--- setup_client.py
+++ setup_client.py
@@ -22,8 +22,17 @@ import linien
 
 assert linien.__version__ != "dev"
 
+long_description = """
+
+    ---
+    **DEPRICATION NOTE**
+
+    This package has been moved to [linien-client](https://pypi.org/project/linien-client/) as of Linien v0.6.0. Please have a look at the [project repository](https://github.com/linien-org/linien) for up to date installation instructions.
+
+    ---
+    """
 with open("README.md", "r") as fh:
-    long_description = fh.read()
+    long_description += fh.read()
 
 setuptools.setup(
     name="linien-python-client",
