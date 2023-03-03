c755439369aacefde269dddee8ba1e7b60cdf053
Bastian Leykauf <leykauf@physik.hu-berlin.de>
2023-03-03 15:49:03 +0100

    use constant for output channel number

--- linien-server/linien_server/acquisition_process.py
+++ linien-server/linien_server/acquisition_process.py
@@ -40,8 +40,8 @@ def shutdown():
 
 class DataAcquisitionService(Service):
     def __init__(self):
-        self.r = RedPitaya()
-        self.csr = PythonCSR(self.r)
+        self.red_pitaya = RedPitaya()
+        self.csr = PythonCSR(self.red_pitaya)
         self.csr_queue = []
         self.csr_iir_queue = []
 
@@ -54,11 +54,11 @@ class DataAcquisitionService(Service):
 
         self.locked = False
         self.exposed_set_sweep_speed(9)
-        # when self.locked is set to True, this doesn't mean that the lock is
-        # really on. It just means that the lock is requested and that the
-        # gateware waits until the sweep is at the correct position for the lock.
-        # Therefore, when self.locked is set, the acquisition process waits for
-        # confirmation from the gateware that the lock is actually running.
+        # when self.locked is set to True, this doesn't mean that the lock is really on.
+        # It just means that the lock is requested and that the gateware waits until the
+        # sweep is at the correct position for the lock. Therefore, when self.locked is
+        # set, the acquisition process waits for confirmation from the gateware that the
+        # lock is actually running.
         self.confirmed_that_in_lock = False
 
         self.fetch_additional_signals = True
@@ -97,7 +97,7 @@ class DataAcquisitionService(Service):
 
                 # copied from https://github.com/RedPitaya/RedPitaya/blob/14cca62dd58f29826ee89f4b28901602f5cdb1d8/api/src/oscilloscope.c#L115  # noqa: E501
                 # check whether scope was triggered
-                not_triggered = (self.r.scope.read(0x1 << 2) & 0x4) > 0
+                not_triggered = (self.red_pitaya.scope.read(0x1 << 2) & 0x4) > 0
                 if not_triggered:
                     sleep(0.05)
                     continue
@@ -105,9 +105,9 @@ class DataAcquisitionService(Service):
                 data, is_raw = self.read_data()
 
                 if self.acquisition_paused:
-                    # it may seem strange that we check this here a second time.
-                    # Reason: `read_data` takes some time and if in the mean time
-                    # acquisition was paused, we do not want to send the data
+                    # it may seem strange that we check this here a second time. Reason:
+                    # `read_data` takes some time and if in the mean time acquisition
+                    # was paused, we do not want to send the data
                     continue
 
                 if self.skip_next_data:
@@ -128,19 +128,19 @@ class DataAcquisitionService(Service):
         if not self.locked:
             target_decimation = 2 ** (self.sweep_speed + int(np.log2(DECIMATION)))
 
-            self.r.scope.data_decimation = target_decimation
-            self.r.scope.trigger_delay = int(trigger_delay / DECIMATION) - 1
+            self.red_pitaya.scope.data_decimation = target_decimation
+            self.red_pitaya.scope.trigger_delay = int(trigger_delay / DECIMATION) - 1
 
         elif self.raw_acquisition_enabled:
-            self.r.scope.data_decimation = 2**self.raw_acquisition_decimation
-            self.r.scope.trigger_delay = trigger_delay
+            self.red_pitaya.scope.data_decimation = 2**self.raw_acquisition_decimation
+            self.red_pitaya.scope.trigger_delay = trigger_delay
 
         else:
-            self.r.scope.data_decimation = 1
-            self.r.scope.trigger_delay = int(trigger_delay / DECIMATION) - 1
+            self.red_pitaya.scope.data_decimation = 1
+            self.red_pitaya.scope.trigger_delay = int(trigger_delay / DECIMATION) - 1
 
         # trigger_source=6 means external trigger positive edge
-        self.r.scope.rearm(trigger_source=6)
+        self.red_pitaya.scope.rearm(trigger_source=6)
 
     def exposed_return_data(self, last_hash):
         no_data_available = self.data_hash is None
@@ -152,8 +152,8 @@ class DataAcquisitionService(Service):
 
     def exposed_set_sweep_speed(self, speed):
         self.sweep_speed = speed
-        # if a slow acqisition is currently running and we change the sweep speed
-        # we don't want to wait until it finishes
+        # if a slow acqisition is currently running and we change the sweep speed we
+        # don't want to wait until it finishes
         self.program_acquisition_and_rearm()
 
     def exposed_set_lock_status(self, locked):
@@ -184,18 +184,18 @@ class DataAcquisitionService(Service):
     def exposed_continue_acquisition(self, uuid):
         self.program_acquisition_and_rearm()
         sleep(0.01)
-        # resetting data here is not strictly required but we want to be on the
-        # safe side
+        # resetting data here is not strictly required but we want to be on the safe
+        # side
         self.data_hash = None
         self.data = None
         self.acquisition_paused = False
         self.data_uuid = uuid
-        # if we are sweeping, we have to skip one data set because an incomplete
-        # sweep may have been recorded. When locked, this does not matter
+        # if we are sweeping, we have to skip one data set because an incomplete sweep
+        # may have been recorded. When locked, this does not matter
         self.skip_next_data = not self.confirmed_that_in_lock
 
     def read_data(self):
-        write_pointer = self.r.scope.write_pointer_trigger
+        write_pointer = self.red_pitaya.scope.write_pointer_trigger
 
         if self.raw_acquisition_enabled:
             return self.read_data_raw(0x10000, write_pointer, MAX_N_POINTS), True
@@ -255,10 +255,10 @@ class DataAcquisitionService(Service):
         #   2'h0,adc_b_rd,2'h0,adc_a_rd
         #   i.e.: 2 zero bits, channel b (14 bit), 2 zero bits, channel a (14 bit)
         # .copy() is required, because np.frombuffer returns a readonly array
-        raw_data = self.r.scope.reads(offset + (4 * addr), data_length).copy()
+        raw_data = self.red_pitaya.scope.reads(offset + (4 * addr), data_length).copy()
 
-        # raw_data is an array of 32-bit ints
-        # we cast it to 16 bit --> each original int is split into two ints
+        # raw_data is an array of 32-bit ints. We cast it to 16 bit --> each original
+        # int is split into two ints
         raw_data.dtype = np.int16
 
         # sign bit is at position 14, but we have 16 bit ints
