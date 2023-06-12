1227701fc2c5c83b235166242d70e674bd37de37
Bastian Leykauf <leykauf@physik.hu-berlin.de>
2023-06-12 17:56:39 +0200

    fix syntax error

--- linien-common/linien_common/config.py
+++ linien-common/linien_common/config.py
@@ -23,7 +23,7 @@ DEFAULT_SWEEP_SPEED = (125 * 2048) << 6
 # IMPORTANT: DEFAULT_COLORS and N_COLORS have to be here, not in client.config
 # because the server needs them and shouldn't import client config as it requires
 # additional packages
-DEFAULT_COLORS: List[Tuple[int, int, int, int]] = [
+DEFAULT_COLORS = [
     (200, 0, 0, 200),
     (0, 200, 0, 200),
     (0, 0, 200, 200),
