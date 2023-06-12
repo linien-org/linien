07e2f1bf1156c6b16ad4d69ae2c6a633764fe764
Bastian Leykauf <leykauf@physik.hu-berlin.de>
2023-06-12 20:15:17 +0200

    fix robust autolock

--- linien-server/linien_server/autolock/robust.py
+++ linien-server/linien_server/autolock/robust.py
@@ -148,7 +148,7 @@ class RobustAutolock:
             * sweep_speed_to_time(self.parameters.sweep_speed.value)
         )
 
-        self.parameters.ping.on_change(self.check_for_timeout)
+        self.parameters.ping.add_listener(self.check_for_timeout)
 
     def check_for_timeout(self, ping):
         min_time_to_wait = 5
