ac63bcb5d8a0e050234fc8012c41f1c6af0b3649
Bastian Leykauf <leykauf@physik.hu-berlin.de>
2023-07-05 07:46:34 +0000

    wait for gateware to be flashed

--- linien-server/linien_server/acquisition.py
+++ linien-server/linien_server/acquisition.py
@@ -308,15 +308,17 @@ class AcquisitionService(Service):
 def flash_fpga():
     filepath = Path(__file__).resolve().parent / "linien.bin"
 
-    # on redpitaya os < 2, flashing fpga works by copying the bit file
-    # /dev/xdevcfg. On recent versions, there is a dedicated command for this
+    # On redpitaya os < 2, flashing fpga works by copying the bit file /dev/xdevcfg. On
+    # recent versions, there is a dedicated command for this
     # cf. https://forum.redpitaya.com/viewtopic.php?p=33494&sid=5132bf6e33709b1a7daa948f8e8dcdb1#p33494  # noqa: E501
     fpga_dev_file = Path("/dev/xdevcfg")
 
     if fpga_dev_file.exists():
+        print("Copying gateware to %s" % fpga_dev_file)
         shutil.copy(str(filepath), str(fpga_dev_file))
     else:
-        subprocess.Popen(["fpgautil", "-b", str(filepath)])
+        print("Using fpautil to deploy gateware.")
+        subprocess.Popen(["fpgautil", "-b", str(filepath)]).wait()
 
 
 def start_nginx():
