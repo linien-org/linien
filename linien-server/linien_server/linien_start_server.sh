381fd8843dad2240c3137435b1637f3e7397bb37
Bastian Leykauf <leykauf@physik.hu-berlin.de>
2023-12-21 14:51:53 +0000

    simplify server startup scripts

--- linien-server/linien_server/linien_start_server.sh
+++ linien-server/linien_server/linien_start_server.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
 # quit any remaining screen session
-if [ -x "$(command -v linien_stop_server.sh)" ]; then
-    linien_stop_server.sh
-fi
-screen -X -S linien-server quit
-# start a new one
-screen -S linien-server -d -m
+linien_stop_server.sh
 
 # stop ethernet blinking and start the server inside the screen session. Start ethernet blinking again after server stopped.
 # Regarding ethernet blinking, see https://github.com/RedPitaya/RedPitaya/issues/205
-screen -r linien-server -X stuff $"mdio-tool w eth0 0x1b 0x0000; linien-server $1; mdio-tool w eth0 0x1b 0x0f00; \n"
+screen -dmS linien-server bash -c "mdio-tool w eth0 0x1b 0x0000;linien-server;mdio-tool w eth0 0x1b 0x0f00"
