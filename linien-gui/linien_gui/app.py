ee5c01f30597d4cfa1bfbe75308c03c982f8925f
Bastian Leykauf <leykauf@physik.hu-berlin.de>
2023-12-29 12:22:12 +0100

    turn startyo for gui into python-fire cli

--- linien-gui/linien_gui/app.py
+++ linien-gui/linien_gui/app.py
@@ -19,7 +19,7 @@ import logging
 import signal
 import sys
 
-import click
+import fire
 from linien_client.connection import LinienClient
 from linien_gui import __version__
 from linien_gui.config import UI_PATH, load_settings
@@ -108,18 +108,23 @@ class LinienApp(QtWidgets.QApplication):
             QtCore.QTimer.singleShot(1000 * 60 * 60, self.check_for_new_version)
 
 
-# ignore type, otherwise "Argument 1 has incompatible type "Callable[[int, bool, str |
-# None, bool], Any]"; expected <nothing>" is raised for click 8.1.4.
-@click.command("linien")  # type: ignore[arg-type]
-@click.version_option(__version__)
-def run_application():
-    app = LinienApp(sys.argv)
-    logger.info("Starting Linien GUI")
+def linien_gui_cli(version=False):
+    """Start the Linien GUI."""
+    if version:
+        print(__version__)
+        return
+    else:
+        app = LinienApp(sys.argv)
+        logger.info("Starting Linien GUI")
 
-    # catch ctrl-c and shutdown
-    signal.signal(signal.SIGINT, signal.SIG_DFL)
-    sys.exit(app.exec_())
+        # catch ctrl-c and shutdown
+        signal.signal(signal.SIGINT, signal.SIG_DFL)
+        sys.exit(app.exec_())
+
+
+def main() -> None:
+    fire.Fire(linien_gui_cli)
 
 
 if __name__ == "__main__":
-    run_application()
+    main()
