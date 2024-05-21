a04fd59f5ff43ddd17eed5aaf02db3a8f3c9dfb9
Bastian Leykauf <leykauf@physik.hu-berlin.de>
2024-05-21 13:53:58 +0200

    adapt acquisition process

--- linien-server/linien_server/acquisition.py
+++ linien-server/linien_server/acquisition.py
@@ -180,6 +180,12 @@ class AcquisitionService(Service):
             if not self.dual_channel and len(signals) >= 3:
                 signals_named["monitor_signal"] = signals[2]
 
+            if self.confirmed_that_in_lock:
+                lock_lost = bool(self.csr.get("logic_relock_watcher_lock_lost"))
+            else:
+                lock_lost = False
+            signals_named["lock_lost"] = lock_lost
+
         slow_out = self.csr.get("logic_slow_value")
         slow_out = slow_out if slow_out <= 8191 else slow_out - 16384
         signals_named["slow_control_signal"] = slow_out
