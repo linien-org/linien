937cd16cb5b12b9a80e4918f6f23c6ffbc385e97
Bastian Leykauf <leykauf@physik.hu-berlin.de>
2024-08-14 15:23:46 +0200

    simplify copyright statements

--- linien-gui/linien_gui/ui/view_panel.py
+++ linien-gui/linien_gui/ui/view_panel.py
@@ -1,7 +1,6 @@
-# Copyright 2018-2022 Benjamin Wiegand <benjamin.wiegand@physik.hu-berlin.de>
-# Copyright 2021-2022 Bastian Leykauf <leykauf@physik.hu-berlin.de>
-#
 # This file is part of Linien and based on redpid.
+#
+# Copyright (C) 2016-2024 Linien Authors (https://github.com/linien-org/linien#license)
 #
 # Linien is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
