a39ab2548fe288a0a847a573a73fe17e9b4e73b7
Bastian Leykauf <leykauf@physik.hu-berlin.de>
2024-09-05 15:54:11 +0200

    fix algorithm selection test

--- tests/test_algorithm_selection.py
+++ tests/test_algorithm_selection.py
@@ -25,6 +25,9 @@ from linien_server.autolock.simple import SimpleAutolock
 from linien_server.server import FakeRedPitayaControlService
 
 Y_SHIFT = 4000
+N_POINTS = 16384
+LOW_JITTER = 10 / 8191
+HIGH_JITTER = 1000 / 8191
 
 
 def peak(x):
@@ -40,74 +43,61 @@ def spectrum_for_testing(x):
 def get_signal(sweep_amplitude, center, shift):
     max_val = np.pi * 5 * sweep_amplitude
     new_center = center + shift
-    x = np.linspace((-1 + new_center) * max_val, (1 + new_center) * max_val, 16384)
+    x = np.linspace((-1 + new_center) * max_val, (1 + new_center) * max_val, N_POINTS)
     return spectrum_for_testing(x)
 
 
 def test_forced_algorithm_selection():
-    def _get_signal(shift):
-        return get_signal(1, 0, shift)
 
-    for forced in (AutolockMode.SIMPLE, AutolockMode.ROBUST):
+    reference_signal = get_signal(1, 0, shift=0)
+    center = int(N_POINTS / 2)
+    x0 = int(center - (0.01 * N_POINTS))
+    x1 = int(center + (0.01 * N_POINTS))
+    next_signal = get_signal(1, 0, shift=0.1)
+
+    for mode_preference in (AutolockMode.SIMPLE, AutolockMode.ROBUST):
         control = FakeRedPitayaControlService()
         parameters = control.parameters
-        parameters.autolock_mode_preference.value = forced
-
-        reference_signal = _get_signal(0)
+        parameters.autolock_mode_preference.value = mode_preference
         autolock = Autolock(control, parameters)
+        autolock.run(x0, x1, reference_signal, auto_offset=True)
 
-        ref_shift = 0
-        N = len(reference_signal)
-        new_center_point = int((N / 2) - ((ref_shift / 2) * N))
-
-        autolock.run(
-            int(new_center_point - (0.01 * N)),
-            int(new_center_point + (0.01 * N)),
-            reference_signal,
-            auto_offset=True,
+        parameters.to_plot.value = pickle.dumps(
+            {"error_signal_1": next_signal, "error_signal_2": []}
         )
 
-        assert autolock.algorithm_selector.mode == forced
-        assert parameters.autolock_mode.value == forced
+        assert autolock.algorithm_selector.mode == mode_preference
+        assert parameters.autolock_mode.value == mode_preference
 
-        if forced == AutolockMode.SIMPLE:
+        if mode_preference == AutolockMode.SIMPLE:
             assert isinstance(autolock.algorithm, SimpleAutolock)
         else:
             assert isinstance(autolock.algorithm, RobustAutolock)
 
 
 def test_automatic_algorithm_selection():
-    def _get_signal(shift):
-        return get_signal(1, 0, shift)
 
-    LOW_JITTER = 10 / 8191
-    HIGH_JITTER = 1000 / 8191
+    reference_signal = get_signal(1, 0, shift=0)
+    center = int(N_POINTS / 2)
+    x0 = int(center - (0.01 * N_POINTS))
+    x1 = int(center + (0.01 * N_POINTS))
+    reference_signal = get_signal(1, 0, shift=0)
+
     for jitter in (LOW_JITTER, HIGH_JITTER):
-        print(f"jitter {jitter}")
         control = FakeRedPitayaControlService()
         parameters = control.parameters
         parameters.autolock_mode_preference.value = AutolockMode.AUTO_DETECT
 
-        reference_signal = _get_signal(0)
         autolock = Autolock(control, parameters)
-
-        ref_shift = 0
-        N = len(reference_signal)
-        new_center_point = int((N / 2) - ((ref_shift / 2) * N))
-
-        autolock.run(
-            int(new_center_point - (0.01 * N)),
-            int(new_center_point + (0.01 * N)),
-            reference_signal,
-            auto_offset=True,
-        )
+        autolock.run(x0, x1, reference_signal, auto_offset=True)
 
         assert autolock.algorithm_selector.mode == AutolockMode.AUTO_DETECT
 
-        for _ in range(10):
-            error_signal = _get_signal(jitter)[:]
+        next_signal = get_signal(1, 0, jitter)[:]
+
+        for next_signal in 10 * [get_signal(1, 0, jitter)[:]]:
             parameters.to_plot.value = pickle.dumps(
-                {"error_signal_1": error_signal, "error_signal_2": []}
+                {"error_signal_1": next_signal, "error_signal_2": []}
             )
 
         if jitter == LOW_JITTER:
